14901065e6e1e58142efb132d158505eba9b8ea2
M.Tanveer Khan <tanveer.khan2692000@gmail.com>
2021-12-14 16:41:17 +0500

    login issue resolved

--- mongoDB.js
+++ mongoDB.js
@@ -4,5 +4,8 @@ const connectToDatabase = () => {
   mongoose.connect(process.env.MONGODBURL, () => {
     console.log("connect to mongoDB Database");
   });
+  mongoose.connection.on("error", (err) => {
+    console.log(err);
+  });
 };
 export default connectToDatabase;
